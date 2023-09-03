0410a23d754f20f9a09db7020753463e749897a5
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-09-03 12:02:52 +0530

    dependency instaled

---
+++ src/Component/Home.jsx
@@ -0,0 +1,34 @@
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Full Stack Developer</h2>
+          <p>
+            Dedicated Full Stack Developer with 1 year of hands-on experience,
+            specializing in React.js as a primary skill and Core Java with
+            Spring Boot as secondary expertise. Proficient in crafting intuitive
+            and responsive web interfaces using React.js and adept at building
+            robust server-side applications. Committed to delivering
+            high-quality solutions, I excel in problem-solving and thrive in
+            collaborative, Agile environments. My ability to bridge the gap
+            between frontend and back-end development ensures seamless,
+            end-to-end application functionality
+          </p>
+          <div>
+            <link>
+                Portfolio
+                <span>
+
+                </span>
+            </link>
+          </div>
+        </div>
+        <div>
+            <img/>
+        </div>
+      </div>
+    </div>
+  );
+};
